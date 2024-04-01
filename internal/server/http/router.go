218174b555237c6fcd6419b1a2fa7b8a09fbaddd
korablin-ivan <ikorablin@tehzor.ru>
2024-04-01 10:45:43 +0300

    commit

--- internal/server/http/router.go
+++ internal/server/http/router.go
@@ -27,7 +27,7 @@ func NewRouter(cfg *config.Config, log *slog.Logger) *Router {
 	// r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(60 * time.Second))
-	r.Use(middleware.AllowContentType("text/plain", "application/json"))
+	r.Use(middleware.AllowContentType("text/plain", "application/json", "application/x-gzip"))
 	r.Use(hm.LogAndGzip)
 
 	r.Post("/", h.CreateURLHandler)
